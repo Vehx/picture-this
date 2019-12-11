9c8df54bafa789fd733f452a26ca050a8e645a8f
vehx <delta222@gmail.com>
2019-12-11 13:01:05 +0100

    Added  comments to login.php

--- app/users/login.php
+++ app/users/login.php
@@ -9,38 +9,40 @@ if (isset($_SESSION['user'])) {
     redirect('/');
 }
 
+// this makes sure email and password is sent and is correct
+// if either is incorrect it redirects the user back to login with an appropriate error
 if (isset($_POST['email'], $_POST['password'])) {
     $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
     $password = $_POST['password'];
 
+    // make sure email isn't an empty string
     if ($email = '') {
         $_SESSION['errors'][] = 'Error: Please enter a valid email.';
+        redirect('/login.php');
     }
 
+    // calls database to see if theres a user with given email
     $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->execute();
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
+    // checks if a user was not found in the database
+    // redirects with error if user was not found
     if (!$user) {
-        // todo send error msg
         $_SESSION['errors'][] = "Error: incorrect email and/or password.";
-        $_SESSION['errors'][] = 'Error2: Please enter a valid email.';
-        $_SESSION['errors'][] = 'Error3: Please enter a valid email.';
-        // die(var_dump($_SESSION));
-
         redirect('/login.php');
     }
 
-    $hashedPassword = $user['password'];
-
-    if (password_verify($password, $hashedPassword)) {
-        // die(var_dump($user));
+    // verifies that given password is correct
+    // otherwise redirects with same error as if email is incorrect for security purposes
+    if (password_verify($password, $user['password'])) {
+        // password is deleted from user array and then user is saved in session for use across the site
         unset($user['password']);
         $_SESSION['user'] = $user;
     } else {
-        // todo implement something here that happens when unsuccessful login attempt happens, maybe do it in : !$user if, above
-        die(var_dump('Error incorrect email and/or password.'));
+        $_SESSION['errors'][] = "Error: incorrect email and/or password.";
+        redirect('/login.php');
     }
 }
 
