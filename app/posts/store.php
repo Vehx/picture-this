8592d0ac4452786482e319466c15aa0872d2a39d
vehx <delta222@gmail.com>
2019-12-13 17:24:22 +0100

    added base of image upload logic

--- app/posts/store.php
+++ app/posts/store.php
@@ -4,4 +4,28 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 // In this file we store/insert new posts in the database.
 
+if (isset($_POST['title'], $_FILES['image'])) {
+    $title = filter_var(trim($_POST['title']), FILTER_SANITIZE_STRING);
+    $image = $_FILES['image'];
+    if (isset($_POST['hashtags'])) {
+        $hashtags = filter_var(trim($_POST['hashtags']), FILTER_SANITIZE_STRING);
+    }
+    // die(var_dump($title, $image));
+
+    if ($image['size'] > 2000000) {
+        $_SESSION['errors'][] = "It's too big!";
+    }
+    if ($image['type'] !== 'image/png' && $image !== 'image/jpg' && $image !== 'image/jpeg') {
+        $_SESSION['errors'][] = "Thats not a valid file type!";
+    }
+
+    if ($_SESSION['errors']) {
+        die(var_dump($_SESSION));
+    }
+    move_uploaded_file(
+        $image['tmp_name'],
+        '../database/uploads/posts/' . $image['name']
+    );
+}
+// die(var_dump($_FILES));
 redirect('/');
--- index.php
+++ index.php
@@ -7,17 +7,18 @@
 
 <?php if (isset($_SESSION['user'])) : ?>
     <section>
-        <form action="submit" method="POST" class="post-form hidden">
+        <form action="app/posts/store.php" method="POST" enctype="multipart/form-data" class="post-form hidden">
             <label for="title">Title : </label>
             <input type="text" name="title" id="title" required>
             <label for="hashtags">Hashtags (optional) : </label>
             <input type="text" name="hashtags" id="hashtags">
             <label for="image">Image : </label>
-            <input type="file" name="image" id="image" required>
-            <button class="btn btn-primary submit-post-btn">Submit post</button>
+            <input type="file" name="image" id="image" accept="jpeg, jpg, png" required>
+            <button type="submit" class="btn btn-primary submit-post-btn">Submit post</button>
         </form>
         <button class="btn btn-primary create-post-btn">Create post</button>
     </section>
 <?php endif; ?>
+<?php showErrors(); ?>
 
 <?php require __DIR__ . '/views/footer.php'; ?>
