b402700747c4e833e170b8d606018abf28a82b62
vehx <delta222@gmail.com>
2019-12-16 13:26:46 +0100

    Added logic to store posts in database

--- app/posts/store.php
+++ app/posts/store.php
@@ -8,12 +8,13 @@ if (isset($_POST['title'], $_FILES['image'])) {
     $title = filter_var(trim($_POST['title']), FILTER_SANITIZE_STRING);
     $image = $_FILES['image'];
     $hashtagsExist = false;
+    $uuidGen = [];
 
+    // checks if any keywords/hashtags were entered and sets a bool to true for use in the database storage code
     if (isset($_POST['hashtags'])) {
         $hashtags = filter_var(trim($_POST['hashtags']), FILTER_SANITIZE_STRING);
         $hashtagsExist = true;
     }
-    // die(var_dump($title, $image));
 
     // checks size and type of image and creates errors if wrong and redirects back with error message
     if ($image['size'] > 2000000) {
@@ -31,10 +32,9 @@ if (isset($_POST['title'], $_FILES['image'])) {
     // image name is set to a uuid before stored in uploads folder and database
     $uuidGen = explode('.', $image['name']);
     $uuidGen[0] = guidv4();
-    $image['name'] = $uuidGen[0] . '.' . $uuidGen[1];
 
-    // the image path is set with its name and saved in variable for use when storing information in database
-    $imagePath = '../database/uploads/posts/' . $image['name'];
+    // the image path is set with its uuid name and saved in variable for use when storing information in database
+    $imagePath = '../database/uploads/posts/' . $uuidGen[0] . '.' . $uuidGen[1];
 
     // image is moved to storage in file structure
     move_uploaded_file(
@@ -42,17 +42,14 @@ if (isset($_POST['title'], $_FILES['image'])) {
         $imagePath
     );
 
-    die(var_dump($imagePath, $userId, $title, $hashtags, $image));
-
     // information about post is saved in database, note keywords/hashtags is optional
-    //     $statement = $pdo->prepare('INSERT INTO posts (user_id, title, picture, keywords) VALUES (:user_id, :title, :picture, :keywords)');
-    //     $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
-    //     $statement->bindParam(':title', $title, PDO::PARAM_STR);
-    //     $statement->bindParam(':picture', $imagePath, PDO::PARAM_STR);
-    //     if ($hashtagsExist) {
-    //         $statement->bindParam(':keywords', $hashtags, PDO::PARAM_STR);
-    //     }
-    //     $statement->execute();
+    $statement = $pdo->prepare('INSERT INTO posts (user_id, title, picture, keywords) VALUES (:user_id, :title, :picture, :keywords)');
+    $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
+    $statement->bindParam(':title', $title, PDO::PARAM_STR);
+    $statement->bindParam(':picture', $imagePath, PDO::PARAM_STR);
+    if ($hashtagsExist) {
+        $statement->bindParam(':keywords', $hashtags, PDO::PARAM_STR);
+    }
+    $statement->execute();
 }
-// die(var_dump($_FILES));
 redirect('/');
