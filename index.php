737fd83f89790b74463cb45ff3bbd729d99b2a22
vehx <delta222@gmail.com>
2019-12-17 12:15:39 +0100

    updated files to naming with crud and post__

--- index.php
+++ index.php
@@ -7,16 +7,16 @@
 
 <?php if (isset($_SESSION['user'])) : ?>
     <section>
-        <form action="app/posts/store.php" method="POST" enctype="multipart/form-data" class="post-form hidden">
+        <form action="app/posts/create.php" method="POST" enctype="multipart/form-data" class="post__form hidden">
             <label for="title">Title : </label>
             <input type="text" name="title" id="title" required>
             <label for="hashtags">Hashtags (optional) : </label>
             <input type="text" name="hashtags" id="hashtags">
             <label for="image">Image : </label>
             <input type="file" name="image" id="image" accept="jpeg, jpg, png" required>
-            <button type="submit" class="btn btn-primary submit-post-btn">Submit post</button>
+            <button type="submit" class="btn btn-primary post__submit-btn">Submit post</button>
         </form>
-        <button class="btn btn-primary create-post-btn">Create post</button>
+        <button class="btn btn-primary post__create-btn">Create post</button>
     </section>
     <section class="post__container"></section>
     <script src="assets/scripts/posts.js"></script>
