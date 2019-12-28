2ea0e04396d0fff63fc5e3ed033a52b6b6ffd168
vehx <delta222@gmail.com>
2019-12-28 16:16:04 +0100

    added psudocode for updating profile info

--- app/users/update.php
+++ app/users/update.php
@@ -2,8 +2,25 @@
 
 declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
-// In this file we delete new posts in the database.
+// In this file we update a users information in the database.
 
 if (!isset($_SESSION['user'])) {
     redirect('/');
 }
+
+
+// if ( name != user name) {
+    // database write new name 
+// }
+// if ( email != user email) {
+    // database write new email 
+// }
+// if ( avatar != user avatar) {
+    // database write new avatar 
+// }
+// if ( biography != user biography) {
+    // database write new biography 
+// }
+// if ( password != verify user password) {
+    // database write new password 
+// }
