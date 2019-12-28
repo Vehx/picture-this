5df0c32e319b265c202cfde41aa8a21ed01905ee
vehx <delta222@gmail.com>
2019-12-28 12:58:17 +0100

    started on logic for viewing profile

--- app/users/read.php
+++ app/users/read.php
@@ -1,14 +1,31 @@
 <?php
 
 declare(strict_types=1);
-require __DIR__ . '/../autoload.php';
 // In this file we delete new posts in the database.
 
 if (!isset($_SESSION['user'])) {
     redirect('/');
 }
 
-if (isset($_SESSION['uid'])) {
-    echo $_SESSION['uid']['name'];
-    unset($_SESSION['uid']['name']);
+if (isset($_GET['uid'])) {
+    $ownProfile = false;
+    $statement = $pdo->prepare('SELECT * FROM users WHERE id = :id');
+    $statement->bindParam(':id', $_GET['uid'], PDO::PARAM_INT);
+
+    $statement->execute();
+} else {
+    $ownProfile = true;
+    $statement = $pdo->prepare('SELECT * FROM users WHERE id = :id');
+    $statement->bindParam(':id', $_SESSION['user']['id'], PDO::PARAM_INT);
+
+    $statement->execute();
+}
+
+$profile = $statement->fetch(PDO::FETCH_ASSOC);
+
+$_SESSION['profile']['name'] = $profile['name'];
+$_SESSION['profile']['avatar'] = $profile['avatar'];
+$_SESSION['profile']['biography'] = $profile['biography'];
+if ($ownProfile) {
+    $_SESSION['profile']['email'] = $profile['email'];
 }
--- profile.php
+++ profile.php
@@ -1,58 +1,65 @@
 <?php require __DIR__ . '/views/header.php'; ?>
+<?php require __DIR__ . '/app/users/read.php'; ?>
 
 <article>
-    <h1>Profile</h1>
+    <h1>
+        <?php
+        echo $_SESSION['profile']['name'];
+        ?>
+    </h1>
 
-    <form action="app/users/register.php" method="post">
-        <div class="form-group">
-            <label for="name">Name</label>
+    <img src="<?php echo $_SESSION['profile']['avatar']; ?>" alt="<?php echo $_SESSION['profile']['name']; ?>'s profile avatar picture">
+
+    <!-- <form action="app/users/update.php" method="post"> -->
+    <div class="form-group">
+        <!-- <label for="name">Name</label>
             <input class="form-control" type="text" name="name" maxlength="255" placeholder="Best Person" value="
                 <?php
-                if (isset($_SESSION['registering']['name'])) {
-                    echo $_SESSION['registering']['name'];
-                    unset($_SESSION['registering']['name']);
-                }
+                // if (isset($_SESSION['registering']['name'])) {
+                //     echo $_SESSION['registering']['name'];
+                //     unset($_SESSION['registering']['name']);
+                // }
                 ?>" required>
-            <small class="form-text text-muted">Please provide your name. <br>(This will be visable to other users.)</small>
-        </div><!-- /form-group -->
+            <small class="form-text text-muted">Please provide your name. <br>(This will be visable to other users.)</small> -->
+    </div><!-- /form-group -->
 
-        <div class="form-group">
-            <label for="email">Email</label>
+    <div class="form-group">
+        <!-- <label for="email">Email</label>
             <input class="form-control" type="text" name="email" maxlength="255" placeholder="your@email.here" value="
             <?php
-            if (isset($_SESSION['registering']['email'])) {
-                echo $_SESSION['registering']['email'];
-                unset($_SESSION['registering']['email']);
-            }
+            // if (isset($_SESSION['registering']['email'])) {
+            //     echo $_SESSION['registering']['email'];
+            //     unset($_SESSION['registering']['email']);
+            // }
             ?>" required>
-            <small class="form-text text-muted">Please provide your email address.</small>
-        </div><!-- /form-group -->
+            <small class="form-text text-muted">Please provide your email address.</small> -->
+    </div><!-- /form-group -->
 
-        <div class="form-group">
-            <label for="password">Password</label>
+    <div class="form-group">
+        <!-- <label for="password">Password</label>
             <input class="form-control" type="password" name="password" minlength="4" maxlength="255" value="
             <?php
-            if (isset($_SESSION['registering']['password'])) {
-                echo $_SESSION['registering']['password'];
-                unset($_SESSION['registering']['password']);
-            }
+            // if (isset($_SESSION['registering']['password'])) {
+            //     echo $_SESSION['registering']['password'];
+            //     unset($_SESSION['registering']['password']);
+            // }
             ?>" required>
-            <small class="form-text text-muted">Please enter your password (passphrase). <br>Must be 4 characters or longer.</small>
-        </div><!-- /form-group -->
-        <div class="form-group">
-            <label for="password-confirm">Confirm Password</label>
+            <small class="form-text text-muted">Please enter your password (passphrase). <br>Must be 4 characters or longer.</small> -->
+    </div><!-- /form-group -->
+    <div class="form-group">
+        <!-- <label for="password-confirm">Confirm Password</label>
             <input class="form-control" type="password" name="password-confirm" minlength="4" maxlength="255" value="
             <?php
-            if (isset($_SESSION['registering']['passwordConfirm'])) {
-                echo $_SESSION['registering']['passwordConfirm'];
-                unset($_SESSION['registering']['passwordConfirm']);
-            }
+            // if (isset($_SESSION['registering']['passwordConfirm'])) {
+            //     echo $_SESSION['registering']['passwordConfirm'];
+            //     unset($_SESSION['registering']['passwordConfirm']);
+            // }
             ?>" required>
-            <small class="form-text text-muted">Please enter the same as above to confirm your password (passphrase).</small>
-        </div><!-- /form-group -->
+            <small class="form-text text-muted">Please enter the same as above to confirm your password (passphrase).</small> -->
+    </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Save</button>
-    </form>
+    <!-- <button type="submit" class="btn btn-primary">Save</button> -->
+    <!-- </form> -->
     <?php showErrors(); ?>
 </article>
 
