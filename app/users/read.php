f235add9ee7679c5cd69619bdf115cefe576da0f
vehx <delta222@gmail.com>
2019-12-30 10:31:50 +0100

    optimized code slightly in read.php for profile

--- app/users/read.php
+++ app/users/read.php
@@ -1,25 +1,21 @@
 <?php
 
 declare(strict_types=1);
-// In this file we delete new posts in the database.
+// In this file we get info on user profile from the database.
 
 if (!isset($_SESSION['user'])) {
     redirect('/');
 }
 
 if (isset($_GET['uid'])) {
-    $ownProfile = false;
     $profile = getProfile($pdo, $_GET['uid']);
 } else {
-    $ownProfile = true;
     $profile = getProfile($pdo, $_SESSION['user']['id']);
+    $_SESSION['profile']['email'] = $profile['email'];
 }
 
 $_SESSION['profile']['name'] = $profile['name'];
 $_SESSION['profile']['avatar'] = $profile['avatar'];
 $_SESSION['profile']['biography'] = $profile['biography'];
-if ($ownProfile) {
-    $_SESSION['profile']['email'] = $profile['email'];
-}
 
 unset($profile);
