114f579cf7caffb1d2f86153390105171335e520
vehx <delta222@gmail.com>
2020-01-07 14:56:25 +0100

    added change password page

---
+++ password.php
@@ -0,0 +1,26 @@
+<?php require __DIR__ . '/views/header.php'; ?>
+
+<form action="app/users/update.php" method="post">
+
+    <div class="form-group">
+        <label for="email">Current password</label>
+        <input class="form-control" type="password" name="password" minlength="4" maxlength="255">
+        <small class="form-text text-muted">Please enter your password (passphrase). <br>Must be 4 characters or longer.</small>
+    </div><!-- /form-group -->
+
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input class="form-control" type="password" name="new-password" minlength="4" maxlength="255">
+        <small class="form-text text-muted">Please enter your new password (passphrase). <br>Must be 4 characters or longer.</small>
+    </div><!-- /form-group -->
+    <div class="form-group">
+        <label for="password-confirm">Confirm Password</label>
+        <input class="form-control" type="password" name="new-password-confirm" minlength="4" maxlength="255">
+        <small class="form-text text-muted">Please enter the same as above to confirm your new password (passphrase).</small>
+    </div><!-- /form-group -->
+
+    <button type="submit" class="btn btn-primary">Save</button>
+</form>
+
+<?php showErrors(); ?>
+<?php require __DIR__ . '/views/footer.php'; ?>
