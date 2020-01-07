fde39d82c94d75384633998a14fd4be3b63747bf
vehx <delta222@gmail.com>
2020-01-07 16:20:59 +0100

    added change password functionality

--- app/users/update.php
+++ app/users/update.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user'])) {
     redirect('/');
 }
 
-if (isset($_POST)) {
+if (isset($_POST) || isset($_FILES)) {
     $hasChanged = false;
     $id = $_SESSION['user']['id'];
     $oldName = $_SESSION['user']['name'];
@@ -70,11 +70,59 @@ if (isset($_POST)) {
         // checks database for email, to make sure it isn't a duplicate
         $emailCheckUser = checkEmail($pdo, $newEmail);
 
-        // checks that new email is not same as old email, not an empty string and not a duplicate email
+        // checks that new email is not same as old email, not an empty string and not an already existing email
         if (!($newEmail === $oldEmail) && $newEmail != '' && !isset($emailCheckUser['email'])) {
             // sends changes to be made in database
             $hasChanged = true;
             updateDatabase($pdo, $databaseTable, $id, 'email', $newEmail);
+        } else {
+            $_SESSION['errors'][] = "Please enter a valid email.";
+            redirect('/profile.php');
+        }
+    }
+
+    // checks if password is sent
+    if (isset($_POST['password'])) {
+        $password = $_POST['password'];
+        $newPassword = $_POST['new-password'];
+        $newPasswordConfirm = $_POST['new-password-confirm'];
+        $user = getProfile($pdo, $id);
+
+        // checks that current password is correct
+        if (password_verify($password, $user['password'])) {
+
+            // checks that a new password was sent and confirmed
+            if ($newPassword != '' || $newPasswordConfirm != '') {
+
+                // check that password is 4 characters or longer
+                if (strlen($newPassword) < 4 || strlen($newPasswordConfirm) < 4) {
+                    $_SESSION['errors'][] = "Error: Password needs to be 4 characters or longer.";
+                    redirect('/password.php');
+                }
+
+                // check that both passwords are the same to confirm that the user knows what their password will be
+                if (!($newPassword === $newPasswordConfirm)) {
+                    $_SESSION['errors'][] = "Error: Passwords don't match, please try again.";
+                    redirect('/password.php');
+                }
+
+                // password gets hashed before being put in database
+                $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+
+                // sends changes to be made in database
+                $hasChanged = true;
+                updateDatabase($pdo, $databaseTable, $id, 'password', $hashedPassword);
+            } else {
+                // if new password or new password confirm wasn't sent in, user is sent back with an error
+                $_SESSION['errors'][] = "Please enter a new password and confirm it.";
+                unset($user);
+                redirect('/password.php');
+            }
+        } else {
+            // if current password was incorrect, user is sent back with an error
+            $_SESSION['errors'][] = "Current password is incorrect.";
+            unset($user);
+            redirect('/password.php');
         }
     }
 
