f876566d33c2f17a6a4cec54f9937a61ffddb656
vehx <delta222@gmail.com>
2020-01-14 16:13:27 +0100

    added stuff to edit post description

--- app/posts/update.php
+++ app/posts/update.php
@@ -4,4 +4,22 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 // In this file we delete posts in the database.
 
-redirect('/');
+if (!isset($_SESSION['user'])) {
+    redirect('/');
+}
+
+if (isset($_POST['edit-description']) && isset($_POST['post-id'])) {
+    $newDescription = filter_var($_POST['edit-description'], FILTER_SANITIZE_STRING);
+    $postId = filter_var($_POST['post-id'], FILTER_SANITIZE_STRING);
+
+    // gets post from database 
+    $currentPost = getPost($pdo, $postId);
+
+    if ($newDescription != $currentPost['description']) {
+
+        // sends changes to be made in database
+        updateDatabase($pdo, 'posts', $postId, 'description', $newDescription);
+    }
+} else {
+    redirect('/');
+}
